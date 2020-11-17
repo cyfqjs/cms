dc49a4fc7b4f8f5ce33153d122e99bdb7d61299c
unknown <15932286035@163.com>
2020-11-17 14:02:51 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Login from '../components/views/login/index'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect:'/login'
+    }
+    ,
+    {
+      path:'/login',
+      name:'login',
+      component:Login
+    }
+  ]
+})
